36e0eb526c285cd2d1445825da17adc37dcd95ed
iamyaoxi <yosi.pramajaya@gmail.com>
2018-10-14 08:51:24 +0700

    Put all math algorithm in math.go, also add PascalTriangle algorithm

--- src/github.com/iamyaoxi/playground/math.go
+++ src/github.com/iamyaoxi/playground/math.go
@@ -26,3 +26,29 @@ func Fibonacci() func() int {
 		return result
 	}
 }
+
+func PascalTriangle() func() []int {
+	//Pascal Triangle based on the sum of the previous line
+	var previousLine []int
+	var currentLine []int
+
+	return func() []int {
+
+		//Add the first 1 in each line
+		currentLine = make([]int, len(previousLine) + 1)
+		currentLine[0] = 1
+		for i := range previousLine {
+			if i == len(previousLine) - 1 {
+				//Add the last 1 in each line
+				currentLine[i+1] = 1
+				break
+			}
+			//Sum the previous line to each other
+			sum := previousLine[i] + previousLine[i+1]
+			currentLine[i+1] = sum
+		}
+
+		previousLine = currentLine
+		return currentLine
+	}
+}
---
+++ src/github.com/iamyaoxi/playground/math_test.go
@@ -0,0 +1,55 @@
+package playground
+
+import "testing"
+
+func Test(t *testing.T) {
+
+	//Unit Test for Fibonacci function
+	//Expected value is set
+	expectedFibo := [11]int{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55}
+	actualFibo := make([]int, 11)
+
+	fibo := Fibonacci()
+	for i:= 0; i < 11; i++ {
+		//assigning the actual value
+		actualFibo[i] = fibo()
+	}
+
+	for i := range expectedFibo {
+		//Throw error if expected != actual
+		if expectedFibo[i] != actualFibo[i] {
+			t.Errorf("FIBONACCI FAILED -> Expected: %v, Actual: %v", expectedFibo[i], actualFibo[i])
+		}
+	}
+
+	//Unit Test for Pascal Triangle Function
+	//Expected value
+	expectedPascal := [][]int {
+		[]int{1},
+		[]int{1, 1},
+		[]int{1, 2, 1},
+		[]int{1, 3, 3, 1},
+		[]int{1, 4, 6, 4, 1},
+		[]int{1, 5, 10, 10, 5, 1},
+		[]int{1, 6, 15, 20, 15, 6, 1},
+	}
+	var actualPascal [][]int = make([][]int, 14, 20)
+
+	//Start assigning value from PascalTriangle
+	pascal := PascalTriangle()
+	for i:= 0; i < 7; i++ {
+		actualPascal[i] = pascal()
+	}
+
+	//Start testing, line by line
+	for i := range expectedPascal {
+		//Value by value
+		for j := range expectedPascal[i] {
+			//If not the same, throw an error
+			if expectedPascal[i][j] != actualPascal[i][j] {
+				t.Errorf("PASCAL FAILED -> On Line: %v. Expected: %v, Actual: %v", i, expectedPascal[i], actualPascal[i])
+				break
+			}
+		}
+	}
+}
