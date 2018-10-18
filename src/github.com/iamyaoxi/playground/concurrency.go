88b081ec31895afdf9f91d5bf6f6f9b940fc56a7
iamyaoxi <yosi.pramajaya@gmail.com>
2018-10-18 18:17:33 +0700

    Learn Generator & Multiplexing concurrency pattern in Go

--- src/github.com/iamyaoxi/playground/concurrency.go
+++ src/github.com/iamyaoxi/playground/concurrency.go
@@ -1,12 +1,55 @@
 package playground
 
 import "time"
+import "fmt"
 
 //Taken from a boring conversation by Rob Pike
 //Watch: https://youtu.be/f6kdp27TYZs
-func SimpleConversation(converse chan string, timeLimit int) {
-	for i := 0; ; i++{
-		converse <- "I'm saying something"
-		time.Sleep(time.Second)
-	}
+
+//SimpleConversation follows Generator pattern that will generate... simple conversation from a character
+func SimpleConversation(character string) <- chan string {
+	//We make channel string
+	converse := make(chan string) 
+
+	//and we execute goroutine function
+	go func() {
+		//which will do these in a different thread
+
+		//the point is to keep sending us with as many as possible
+		//Until the program exited
+		for i := 0; ; i++ { 
+			//Send the message to the channel
+			converse <- fmt.Sprintf("%v says: %v", character, i)
+			//But can sleep for a second. so we can monitor what happen
+			time.Sleep(time.Second)
+		}
+	}()
+
+	//return the channel as receive only channel
+	return converse
+}
+
+//FanIn follows Multiplexing pattern.
+//It will combine 2 channels into one channel
+func FanIn(ch1, ch2 <- chan string) <- chan string {
+	//First we make channel that holds other channel
+	fanIn := make(chan string)
+
+	//Then here we execute goroutine function
+	go func() { 
+		//This line here means 
+		//Keep receiving and sending any value from the channel 1 to the fanIn channel
+		for {
+			fanIn <- <-ch1
+			}
+	}()
+	go func() { 
+		//Keep receiving and sending any value from the channel 2 to the fanIn channel
+		for {
+			fanIn <- <- ch2
+			}
+	}()
+
+	//Return fanIn channel
+	return fanIn
 }
--- src/github.com/iamyaoxi/playground/concurrency_test.go
+++ src/github.com/iamyaoxi/playground/concurrency_test.go
@@ -4,13 +4,24 @@ import "testing"
 import "fmt"
 
 func TestSimpleConversation(t *testing.T) {
-	c := make(chan string)
-	limit := 5
+	c := SimpleConversation("John")
 
-	go SimpleConversation(c, limit)
-
-	for i:=0; i < limit; i++ {
+	for i:=0; i < 5; i++ {
 		fmt.Println(<- c)
 	}
-	fmt.Println("Ok. I'm leaving. Bye")
+	fmt.Println("Thanks John")
+}
+
+func TestFanIn(t *testing.T) {
+	//Generates 2 channels from SimpleConversation
+	ch1 := SimpleConversation("Annie")
+	ch2 := SimpleConversation("Joe")
+
+	chResult := FanIn(ch1, ch2)
+
+	//Whatever it is, we only want to receive 20 first values from the fanIn channel
+	for i:=0; i < 20; i++ {
+		fmt.Println(<- chResult)
+	}
+	fmt.Println("Thanks Annie and Joe")
 }
